534da339015f02ab02a2cd1d03d277c7baa97eec
jkl273 <tph.jkl@gmail.com>
2017-01-20 12:16:51 +0900

    update

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,17 +1,33 @@
 extern crate hyper;
 extern crate encoding;
+extern crate rand;
+extern crate rustc_serialize;
 
 use hyper::client::Client;
 use std::io::prelude::*;
+use rand::distributions::{IndependentSample, Range};
+use rustc_serialize::json::Json;
 
 use encoding::{Encoding, DecoderTrap};
 use encoding::all::WINDOWS_31J; // shift jis
 
-fn getbook(bid: &str) -> String {
-    let client = Client::new();
+fn getbook(bid: String) -> String {
     let url = "http://www.aozorahack.net/api/v0.1/";
     let url2 = format!("{url}books/{bid}/content", url=url, bid=bid);
-    let mut res = client.get(url2.as_str()).send().unwrap();
+    getaz(url2)
+}
+
+fn getidx(idx: u32) -> u64 {
+    let url = "http://www.aozorahack.net/api/v0.1/";
+    let url2 = format!("{url}books?limit=1&skip={idx}", url=url, idx=idx);
+    let data = Json::from_str(getaz(url2).as_str()).unwrap();
+    let arr = data.as_array().unwrap();
+    arr[0]["book_id"].as_u64().unwrap()
+}
+
+fn getaz(url: String) -> String {
+    let client = Client::new();
+    let mut res = client.get(url.as_str()).send().unwrap();
     assert_eq!(res.status, hyper::Ok);
     println!("{:?}", res);
 
@@ -31,7 +47,14 @@ fn summary(body: String) -> String {
 }
 
 fn main() {
-    let body = getbook("1234");
+    let maxidx = 3;
+    let mut rng = rand::thread_rng();
+    let between = Range::new(0, maxidx);
+    let idx = between.ind_sample(&mut rng);
+    println!("{}", idx);
+    
+    let bookid = getidx(idx);
+    let body = getbook(bookid.to_string());
     let sum = summary(body);
 
     println!("{}", sum)
