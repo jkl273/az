e9a3abdede872eaac2daffd905fb727f1374a4e0
jkl273 <tph.jkl@gmail.com>
2018-12-26 15:47:31 +0900

    update

---
+++ goaz/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "cli-demo",
+	Short: "Short comment",
+	Long:  "Long comment",
+	//Run: func(cmd *cobra.Command, args []string) { },
+}
+
+//Execute is called from main function
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
